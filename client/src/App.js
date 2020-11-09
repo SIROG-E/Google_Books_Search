b886785b6c2348120a83dec0276bda6d51b9fb62
Evelyn Goris <goris.evelyn@gmail.com>
2020-11-08 23:28:41 -0600

    imported  components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Nav from "./components/Nav"
-// import Search from "./pages/Search";
-// import Saved from "./pages/Saved";
-// import Jumbotron from "./components/Jumbotron";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav"
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
-      // <Router>
-      //   <div>
-      //     <Nav />
-      //     <Jumbotron />
-      //     <Switch>
-      //       {/* renders the Search page when "/" route is hit */}
-      //       <Route exact path="/" component={Search} />
-      //       {/* renders the Search page when "/search" route is hit */}
-      //       <Route exact path="/search" component={Search} />
-      //       {/* renders the Saved page when "/saved" route is hit */}
-      //       <Route exact path="/saved" component={Saved} />
-      //     </Switch>
-      //   </div>
-      // </Router>
+      <Router>
+        <div>
+          <Nav />
+          <Jumbotron />
+          <Switch>
+            {/* renders the Search page when "/" route is hit */}
+            <Route exact path="/" component={Search} />
+            {/* renders the Search page when "/search" route is hit */}
+            <Route exact path="/search" component={Search} />
+            {/* renders the Saved page when "/saved" route is hit */}
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
